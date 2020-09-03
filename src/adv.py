36c29a938ab68149268f5e50509ba1aad8f88b8b
whitneyxlachelle <whiggins16@yahoo.com>
2020-09-03 15:51:22 -0500

    Day 1 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+import textwrap
 
 # Declare all the rooms
 
@@ -14,7 +16,7 @@ into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
@@ -37,15 +39,61 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
+# Intro Message
+def intro_message():
+    print("Find Charles!")
+
+intro_message()
+
 # Make a new player object that is currently in the 'outside' room.
+character = input("Hello human. What is your name? ")
+location = room['outside']
+player1 = Player(character, location)
 
 # Write a loop that:
-#
+#-
+while True:
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
+    current_room = player1.current_room
+    print(f"{player1.current_room.description}. You're currently in {player1.current_room.name}. \n")
+
 # * Waits for user input and decides what to do.
-#
 # If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
+    choice = input("You can move [n, e, s, w] or press [q] to quit. Which way would you like to go? \n")
+
 # If the user enters "q", quit the game.
+    if choice == 'q':
+        print("Bye, for now " + character)
+        break
+# Player is outside
+    if choice == 'n':
+        if hasattr(current_room, 'n_to'):
+            player1.current_room = current_room.n_to
+            print("You are now heading north... \n")
+ 
+# Print an error message if the movement isn't allowed.           
+        else: 
+            print(f"{character}, you can't be here. Go Back! \n")
+    
+    elif choice == 'e':
+        if hasattr(current_room, 'e_to'):
+            player1.current_room = current_room.e_to
+            print("You are now heading east... \n")
+        else: 
+            print(f"{character}, you can't be here. Go Back! \n")
+    elif choice == 's':
+        if hasattr(current_room, 's_to'):
+            player1.current_room = current_room.s_to
+            print("You are now heading south... \n")
+        else: 
+            print(f"{character}, you can't be here. Go Back! \n")
+    elif choice == 'w':
+        if hasattr(current_room, 'w_to'):
+            player1.current_room = current_room.w_to
+            print("You are now heading west... \n")
+        else: 
+            print(f"{character}, you can't be here. Go Back! \n")
+
+# hasattr(): (object, name) <-- looks thru object to find specified name. RETURNS True or false
--- src/player.py
+++ src/player.py
@@ -1,7 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
+# Use the __init__() function to assign values to object properties
+
+# Player Attributes: name, current_room
+
 class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+    
+   
